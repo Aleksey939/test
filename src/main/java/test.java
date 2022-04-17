b474092cf9b9d8d17dfc84d2441ad3f890902676
Aleksey Ivanov <aleksey93919@gmail.com>
2022-04-17 15:54:24 +0300

    коментарий

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -11,30 +11,30 @@ public class test {
     public static void main(String[] args) throws InterruptedException, IOException {
 
 
-//
-//        ExecutorService executorService= Executors.newSingleThreadExecutor();
-//        executorService.execute(()->System.out.println(1));
-//        executorService.shutdown();
-//        executorService.awaitTermination(10, TimeUnit.MILLISECONDS);
-//
-//        new Thread(()-> {
-//            for (int i=10;i<12;i++){
-//                System.out.println(i);
-//
-//                try {
-//                    Thread.sleep(1000);
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        }).start();
 
-//        FileWriter fileWriter =null;
-//        fileWriter=new FileWriter("1.txt");
-//        for(int i=0;i<256;i++){
-//        fileWriter.write(i);
-//            System.out.println(i);}
-//        fileWriter.close();
+        ExecutorService executorService= Executors.newSingleThreadExecutor();
+        executorService.execute(()->System.out.println(1));
+        executorService.shutdown();
+        executorService.awaitTermination(10, TimeUnit.MILLISECONDS);
+
+        new Thread(()-> {
+            for (int i=10;i<12;i++){
+                System.out.println(i);
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+        FileWriter fileWriter =null;
+        fileWriter=new FileWriter("1.txt");
+        for(int i=0;i<256;i++){
+        fileWriter.write(i);
+            System.out.println(i);}
+        fileWriter.close();
 
 
     }
